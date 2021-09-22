2c14dd3d946906efdbc3735865427b2c500f78a3
elcinp <elcinpars1@gmail.com>
2021-09-22 22:29:31 +0300

    register.js added

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,11 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid, Container, Paper, Avatar, Typography, TextField, Button, CssBaseline } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import blogImage from '../../assests/blok.png'
 
-const Register = () => {
-    return (
-        <div>
-            <h2>Register Page</h2>
-        </div>
-    )
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		backgroundImage: 'url("https://picsum.photos/1600/900")',
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		height: '100vh'
+	},
+	container: {
+		opacity: '0.8',
+		height: 'max-content',
+		marginTop: theme.spacing(10),
+		[theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
+			marginTop: 0,
+			width: '100%',
+			height: '100%'
+		}
+	},
+	div: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.primary.main
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(1)
+	},
+	button: {
+		margin: theme.spacing(3, 0, 2)
+	},
+    blogImage: {
+        width:'80px',
+        height:'80px'
+    }
+}))
+
+const Login = () => {
+	const [body, setBody] = useState({ email: '', password: '' })
+	const classes = useStyles()
+
+
+	const handleChange = e => {
+		setBody({
+			...body,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const onSubmit = () => {
+		console.log(body)
+	}
+
+	return (
+		<Grid container component='main' className={classes.root}>
+			<CssBaseline />
+			<Container component={Paper} elevation={5} maxWidth='xs' className={classes.container}>
+				<div className={classes.div}>
+					<Avatar className={classes.avatar}>
+						<blogImage className={classes.blogImage}/>
+					</Avatar>
+					<Typography component='h1' variant='h5'>── REGISTER ── </Typography>
+					<form className={classes.form}>
+						<TextField
+							fullWidth
+							autoFocus
+                            type='email'
+							color='primary'
+							margin='normal'
+							variant='outlined'
+							label='Email'
+							name='email'
+							value={body.email}
+							onChange={handleChange}
+						/>
+						<TextField
+							fullWidth
+							type='password'
+							color='primary'
+							margin='normal'
+							variant='outlined'
+							label='Password'
+							name='password'
+							value={body.password}
+							onChange={handleChange}
+						/>
+						<Button
+							fullWidth
+							variant='contained'
+							color='secondary'
+							className={classes.button}
+							onClick={() => onSubmit()}
+						>
+							REGISTER
+						</Button>
+					</form>
+				</div>
+			</Container>
+		</Grid>
+	)
 }
 
-export default Register
+export default Login
+
