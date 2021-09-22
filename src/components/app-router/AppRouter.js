6d870f3324ce5a48fd8527880a40b0c0357d2838
elcinp <elcinpars1@gmail.com>
2021-09-22 14:52:02 +0300

    <Route/> login register page added

--- src/components/app-router/AppRouter.js
+++ src/components/app-router/AppRouter.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { BrowserRouter,Switch } from 'react-router-dom'
+import { BrowserRouter,Switch,Route } from 'react-router-dom'
 import Navbar from '../navbar/Navbar'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
                 <Navbar/>
             <Switch>
-                
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
             </Switch>
         </BrowserRouter>
     )
